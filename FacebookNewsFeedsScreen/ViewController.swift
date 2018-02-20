43b791fcf9d2d536754395c35dd55f6d16abc3ec
Appinventiv <appinventiv@Administrators-Mac-mini.local>
2018-02-20 09:36:59 +0530

    Initial Commit

---
+++ FacebookNewsFeedsScreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FacebookNewsFeedsScreen
+//
+//  Created by Appinventiv on 2/20/18.
+//  Copyright © 2018 Appinventiv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
